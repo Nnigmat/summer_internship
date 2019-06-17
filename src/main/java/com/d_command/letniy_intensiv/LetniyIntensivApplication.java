b04979856d862b6a22457ec980851217a5a19b99
Nikita <nnigmat@gmail.com>
2019-06-17 22:44:04 +0300

    init

---
+++ src/main/java/com/d_command/letniy_intensiv/LetniyIntensivApplication.java
@@ -0,0 +1,13 @@
+package com.d_command.letniy_intensiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetniyIntensivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetniyIntensivApplication.class, args);
+    }
+
+}
