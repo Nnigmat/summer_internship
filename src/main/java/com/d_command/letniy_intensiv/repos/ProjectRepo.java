e84b28285def5100c0fb94297ccb6d0a20314c01
Nikita <nnigmat@gmail.com>
2019-06-18 18:20:34 +0300

    Add repos

---
+++ src/main/java/com/d_command/letniy_intensiv/repos/ProjectRepo.java
@@ -0,0 +1,7 @@
+package com.d_command.letniy_intensiv.repos;
+
+import com.d_command.letniy_intensiv.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepo extends JpaRepository<Project, Long> {
+}
